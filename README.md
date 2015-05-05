3d9289e472192d42f63db088daf801827859ff78
nerdyprog <nerdyprog@github.com>
2015-05-05 15:56:01 +0100

    Add F1 keyboard touch shortcut

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Created by MKL &amp; AR.
 Actually created to talk simply during the course. Your neighbour sees what you're writing on your screen.
 Not a great stuff. But nice when you need to talk during course.
 
-You just have to press esc button to erase the text.
+
+
+Shortcuts :
+- Esc button : erase the text.
+- F1 button : minimize the window
 
 Next step is to create a real chat application. ;)
--- SimpleDialogWindow.java
+++ SimpleDialogWindow.java
@@ -1,13 +1,38 @@
+/**
+    The MIT License (MIT)
+
+    Copyright (c) 2015 
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+ */
 package simpledialogwindow;
 
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.KeyStroke;
+
 
 /**
  *
@@ -15,23 +40,62 @@ import javax.swing.KeyStroke;
  */
 public class SimpleDialogWindow extends JFrame{
     JTextField textField;
+    boolean isIconified=false;
 
     public SimpleDialogWindow() {
+        
         this.setSize(1000,75);
         textField = new JTextField();
         Font font1 = new Font("SansSerif", Font.BOLD, 30);
         textField.setFont(font1);
+        this.setFocusable(true);
+        this.setFocusTraversalKeysEnabled(true);
+        
+        textField.addKeyListener(
+            new KeyListener(){  
+                public void keyPressed(KeyEvent e){
+                    if(e.getKeyCode() == KeyEvent.VK_ESCAPE){
+                        textField.setText("");
+                    }else if(e.getKeyCode() == KeyEvent.VK_F1){
+                        System.out.println("F1 keytouch trough the frame itself");
+                        setExtendedState(JFrame.ICONIFIED);
+                    }else if(e.getKeyCode() == KeyEvent.VK_F2){
+                        System.out.println("F2 keytouch trough the textfield");
+                        setExtendedState(JFrame.NORMAL);
+                    }
+                }
 
-        /* When you click on escape touch, it erases your text */
-        Action action = new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                textField.setText("");
-            }};
-        String keyStrokeAndKey = "ESCAPE";
-        KeyStroke keyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0);
-        textField.getInputMap().put(keyStroke, keyStrokeAndKey);
-        textField.getActionMap().put(keyStrokeAndKey, action);
+                @Override
+                public void keyTyped(KeyEvent e) {
+                    
+                }
+
+                @Override
+                public void keyReleased(KeyEvent e) {}
+            }
+        );
+        
+        this.addKeyListener(
+                new KeyListener(){  
+                public void keyPressed(KeyEvent e){
+                    if(e.getKeyCode() == KeyEvent.VK_F1){
+                        System.out.println("F1 keytouch trough the frame itself");
+                        setExtendedState(JFrame.ICONIFIED);
+                    }else if(e.getKeyCode() == KeyEvent.VK_F2){
+                        System.out.println("F2 keytouch trough the textfield");
+                        setExtendedState(JFrame.NORMAL);
+                    }else if(e.getKeyCode() == KeyEvent.VK_ENTER){
+                        System.out.println("ENTER");
+                    }
+                }
+
+                @Override
+                public void keyTyped(KeyEvent e) {}
+
+                @Override
+                public void keyReleased(KeyEvent e) {}
+            }
+        );
 
         this.getContentPane().add(textField);
         this.setVisible(true);
@@ -47,5 +111,7 @@ public class SimpleDialogWindow extends JFrame{
         // TODO code application logic here
         SimpleDialogWindow sdw = new SimpleDialogWindow();
     }
+
+
 
 }
